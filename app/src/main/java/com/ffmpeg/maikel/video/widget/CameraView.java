2addc12cdeb6388e5e1482bd62fdb079232e4ce3
maikel0maikel <maikel@foxmail.com>
2018-03-13 14:25:34 +0800

    Update CameraView.java
    解决预览变形

--- app/src/main/java/com/ffmpeg/maikel/video/widget/CameraView.java
+++ app/src/main/java/com/ffmpeg/maikel/video/widget/CameraView.java
@@ -1,6 +1,7 @@
 package com.ffmpeg.maikel.video.widget;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -21,16 +22,14 @@ import java.util.List;
 
 public class CameraView extends SurfaceView implements SurfaceHolder.Callback, Camera.PreviewCallback {
     private static final String TAG = CameraView.class.getSimpleName();
-    private static final int BACK_CAMERA = 0;
-    private static final int FRONT_CAMERA = 1;
-    private int cameraId = BACK_CAMERA;
     private Camera mCamera = null;
     private SurfaceHolder mHolder;
     private boolean isPreview = false;
     private int mVideoWidth = 0;
     private int mVideoHeight = 0;
     private CameraPresenter cameraPresenter;
-
+    private Camera.Size mPreviewSize;
+    private int mLastOrientation;
     public CameraView(Context context) {
         super(context);
     }
@@ -65,9 +64,6 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback, C
             mCamera.setPreviewDisplay(mHolder);
         } catch (IOException e) {
             Log.e(TAG, "camera display failure :" + e.getMessage());
-            if (mCamera != null) {
-                mCamera.release();
-            }
             cameraPresenter.destroy();
         }
     }
@@ -77,69 +73,153 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback, C
         stopPreview();
         if (mCamera == null) {
             Log.e(TAG, "<surfaceChanged> camera not found please check!");
-            destroyView();
+            destroyMainView();
             return;
         }
         Camera.Parameters parameters = mCamera.getParameters();
         List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
-        Collections.sort(sizes, new Comparator<Camera.Size>() {
-            @Override
-            public int compare(Camera.Size size1, Camera.Size size2) {
-                return size1.width * size1.height - size2.width * size2.height;
-            }
-        });
-        int count = sizes.size();
-        for (int i = 0; i < count; i++) {
-            Camera.Size size = sizes.get(i);
-            if ((size.width >= mVideoWidth && size.height >= mVideoHeight) || i == count - 1) {
-                mVideoWidth = size.width;
-                mVideoHeight = size.height;
-                break;
-            }
-        }
+//        Collections.sort(sizes, new Comparator<Camera.Size>() {
+//            @Override
+//            public int compare(Camera.Size size1, Camera.Size size2) {
+//                return size1.width * size1.height - size2.width * size2.height;
+//            }
+//        });
+//        int count = sizes.size();
+//        for (int i = 0; i < count; i++) {
+//            Camera.Size size = sizes.get(i);
+//            if ((size.width >= mVideoWidth && size.height >= mVideoHeight) || i == count - 1) {
+//                mVideoWidth = size.width;
+//                mVideoHeight = size.height;
+//                break;
+//            }
+//        }
+        mPreviewSize = getFixedSize(sizes,getMeasuredWidth(),getMeasuredHeight());
         //设置预览的宽度、高度
-        parameters.setPreviewSize(mVideoWidth, mVideoHeight);
+        parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
         //设置帧率30帧，这里应该做成动态帧率根据网络状态
         parameters.setPreviewFrameRate(30);
         mCamera.setParameters(parameters);
         try {
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
+            isPreview = true;
         } catch (IOException e) {
             Log.e(TAG, "<surfaceChanged> camera can not preview!");
-            destroyView();
+            destroyMainView();
         }
     }
+
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         mHolder.removeCallback(this);
         destroy();
-
     }
+
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
         //receive video frame
     }
 
+    /**
+     * 查找合适的size
+     * @param sizes
+     * @param w
+     * @param h
+     * @return
+     */
+    private Camera.Size getFixedSize(List<Camera.Size> sizes, int w, int h) {
+        if (sizes == null) {
+            return null;
+        }
+        Camera.Size fixedSize = null;
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) w / h;
+        double minDiff = Double.MAX_VALUE;
+        int targetHeight = h;
+        //找到比较合适的尺寸
+        for (Camera.Size size:sizes){
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio)>ASPECT_TOLERANCE){
+                continue;
+            }
+            if (Math.abs(size.height - targetHeight)<minDiff){
+                fixedSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+        //没有找到合适的，则忽略ASPECT_TOLERANCE
+        if (fixedSize == null){
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size:sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    fixedSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return fixedSize;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final  int widthSize = resolveSize(getSuggestedMinimumWidth(),widthMeasureSpec);
+        final int heightSize = resolveSize(getSuggestedMinimumHeight(),heightMeasureSpec);
+        setMeasuredDimension(widthSize,heightSize);
+        if (mPreviewSize==null){
+            mPreviewSize = getFixedSize(mCamera.getParameters().getSupportedPreviewSizes(),Math.max(widthSize, heightSize), Math.min(widthSize, heightSize));
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int curOrientation =
+                getContext().getResources().getConfiguration().orientation;
+        if (changed &&  mLastOrientation != curOrientation) {
+            mLastOrientation = curOrientation;
+            final int width = right - left;
+            final int height = bottom - top;
+            int previewWidth = width;
+            int previewHeight = height;
+            if (mPreviewSize != null) {
+                previewWidth = mPreviewSize.width;
+                previewHeight = mPreviewSize.height;
+                if (curOrientation == Configuration.ORIENTATION_PORTRAIT) {
+                    previewWidth = mPreviewSize.height;
+                    previewHeight = mPreviewSize.width;
+                }
+            }
+            // Center the child SurfaceView within the parent.
+            if (width * previewHeight > height * previewWidth) {
+                final int scaledChildWidth = previewWidth * height / previewHeight;
+                layout((width - scaledChildWidth) / 2, 0,
+                        (width + scaledChildWidth) / 2, height);
+            } else {
+                final int scaledChildHeight = previewHeight * width / previewWidth;
+                layout(0, (height - scaledChildHeight) / 2,
+                        width, (height + scaledChildHeight) / 2);
+            }
+        }
+    }
+
     private void stopPreview() {
         if (mCamera != null && isPreview) {
             mCamera.stopPreview();
+            isPreview = false;
         }
     }
 
-    private void destroyView() {
+    private void destroyMainView() {
+        if (cameraPresenter != null) {
+            cameraPresenter.finishMain();
+        }
+    }
+
+    private void destroy() {
+        stopPreview();
         if (cameraPresenter != null) {
             cameraPresenter.destroy();
-        }
-    }
-
-    public void destroy() {
-        stopPreview();
-        if (mCamera != null) {
-            mCamera.setPreviewCallback(null);
-            mCamera.release();
         }
     }
 }
