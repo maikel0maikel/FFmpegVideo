be3ef9ef6febdec400adacdbd9b0c6df84c3d5c9
maikel0maikel <maikel@foxmail.com>
2018-03-13 09:03:48 +0800

    Update CameraPresenterImpl.java
    解决屏幕旋转画像倒置。

--- app/src/main/java/com/ffmpeg/maikel/video/presenter/impl/CameraPresenterImpl.java
+++ app/src/main/java/com/ffmpeg/maikel/video/presenter/impl/CameraPresenterImpl.java
@@ -37,7 +37,32 @@ public class CameraPresenterImpl implements CameraPresenter {
 
     @Override
     public void rotation(int orientation) {
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(mCameraId, info);
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
 
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        mCamera.setDisplayOrientation(result);
     }
 
     @Override
@@ -59,4 +84,6 @@ public class CameraPresenterImpl implements CameraPresenter {
     public void destroy() {
 
     }
+    
+
 }
